1f3c3eeb1b75bb71ced9e4eddda24210003b0533
Pedro <pedrodmbc@gmail.com>
2020-06-28 18:46:35 -0300

    List repositories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  });
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +23,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            <h2>{repository.title}</h2>
+            <p>{repository.url}</p>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
