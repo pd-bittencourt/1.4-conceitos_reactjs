006e1d45c172cc7442a783c261c3f225e13b96ce
Pedro <pedrodmbc@gmail.com>
2020-06-29 10:56:41 -0300

    Catch error no useEffect

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ function App() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    api.get("repositories").then((response) => {
-      setRepositories(response.data);
-    });
+    api
+      .get("repositories")
+      .then((response) => {
+        setRepositories(response.data);
+      })
+      .catch((err) => console.error(err));
   });
 
   async function handleAddRepository() {
